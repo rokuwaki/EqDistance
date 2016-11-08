18c41918209dba718c5ee0e6c4150b40c25c0c32
rokuwaki <rokuwaki@gmail.com>
2016-11-08 19:02:28 +0900

    Update RunEqDistance.sh

--- bin/RunEqDistance.sh
+++ bin/RunEqDistance.sh
@@ -3,7 +3,7 @@ MinMag=3
 Catalog="hs_1981_2011_06_comb_K2_A.cat_so_SCSN_v01"
 
 mkdir -p data
-#wget http://scedc.caltech.edu/ftp/catalogs/hauksson/Socal_DD/$Catalog -P data/
+wget http://scedc.caltech.edu/ftp/catalogs/hauksson/Socal_DD/$Catalog -P data/
 
 echo "Converting calendar time of catalog to UNIX time in [s]"
 bash bin/CalSecUNIXTime.sh data/$Catalog $MinMag > data/$Catalog"_M"$MinMag"_sec".txt
